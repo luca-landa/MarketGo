39fde224d88964ce3e833ba708cd8083eb688ee6
luca-landa <lucalanda@hotmail.it>
2018-05-20 17:32:39 +0200

    first commit

---
+++ bin/start_mongo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#mongod --dbpath ~/mongodb/data/db --logpath ~/mongodb/log/mongo.log --logappend
+mongod --dbpath ~/mongodb/data/db
---
+++ client.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const config = require('./lib/config').config;
+const Server = require('./lib/server').Server;
+
+new Server(config).start();
