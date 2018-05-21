b16645b0b5744de4611ef3fee10214c7d52c51ee
luca-landa <lucalanda@hotmail.it>
2018-05-21 20:47:26 +0200

    * incluse librerie vue e jquery servite in locale

--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
 "use strict";
 
+let devices, webSocketConfig;
+
 function setupPage() {
-    let webSocketConfig = JSON.parse('<!-- @echo webSocketConfig -->');
-    let devices = JSON.parse('<!-- @echo devices -->');
+    webSocketConfig = JSON.parse('<!-- @echo webSocketConfig -->');
+    devices = JSON.parse('<!-- @echo devices -->');
+
+    let app = new Vue({
+        el: '#app',
+        data: {
+            devices: devices
+        }
+    });
 
     let webSocket = new WebSocket('ws://localhost:' + webSocketConfig.port, webSocketConfig.protocol);
 
-    $('#shelf-0').on('change', (ev) => {
-        let newVal = ev.target.value;
-        let msg = {
-            idx: 0,
-            quantity: newVal
-        };
-        webSocket.send(JSON.stringify(msg));
-    });
+    // $('#shelf-0').on('change', (ev) => {
+    //     let newVal = ev.target.value;
+    //     let msg = {
+    //         idx: 0,
+    //         quantity: newVal
+    //     };
+    //     webSocket.send(JSON.stringify(msg));
+    // });
 }
 
 window.onload = setupPage;
