552e1130f7b678ff956c31c27c8a192534e5438a
luca-landa <lucalanda@hotmail.it>
2018-06-01 09:25:24 +0200

    * moved Vue components registration in a separate asset

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -11,6 +11,8 @@ function setupPage() {
 
     let webSocket = createWebSocket(webSocketConfig.port, webSocketConfig.protocol);
 
+    registerVueComponents();
+
     app = new Vue({
         el: '#app',
         data: {
@@ -19,7 +21,6 @@ function setupPage() {
             phoneDragging: false,
             productDragging: false
         },
-        components: getVueComponents(),
         methods: {
             updateShelf(shelf, newQuantity) {
                 if (newQuantity < shelf.minQuantity) {
@@ -92,164 +93,5 @@ function createWebSocket(port, protocol) {
 
     return webSocket;
 }
-
-function getVueComponents() {
-    return {
-        'palmar': {
-            props: ['palmar'],
-            template: `
-            <div class="device palmar">
-                <label class="palmar-label">Palmar {{palmar.idx}}</label>
-                <div class="display">
-                    <h4 v-if="palmar.notifications.length === 0">No notifications to display</h4>
-                    <div class="notification" v-for="notification in palmar.notifications">
-                        <p class="palmar-message">{{notification.action}} (idx "{{notification.idx}}")</p>
-                        <button class="palmar-gui-button success" @click="sendCompletedAction(notification)">done</button>
-                    </div>
-                </div>
-                <div class="home-button" @click="alert('not implemented yet')"></div>
-            </div>`,
-            methods: {
-                sendCompletedAction(notification) {
-                    app.sendCompletedAction(this.palmar, notification);
-                }
-            }
-        },
-        'client-palmar': {
-            props: ['palmar'],
-            template: `
-                <div id="client-palmar" class="device palmar" draggable="true" @dragstart="dragStart($event)"
-                 @dragend="dragEnd($event)">
-                <label class="palmar-label">{{palmar.username}}'s phone</label>
-                <div class="display">
-                    <h4 v-if="palmar.notifications.length === 0">No notifications to display</h4>
-                    <div v-for="notification in palmar.notifications">
-                        <transition appear appear-active-class="new-notification-animation">
-                            <div class="notification">
-                                <div v-if="notification.type === 'message'" >
-                                    <p class="palmar-message">
-                                        <span class="x-close" @click="removeNotification(notification)">&#10006;</span>   
-                                        {{notification.data}}
-                                    </p>
-                                </div>
-                                <div v-else-if="notification.type === 'productInformation'">
-                                    <h4 class="palmar-message"><span class="x-close" @click="removeNotification(notification)">&#10006;</span> Product info</h4>
-                                    <div v-for="warning in notification.warnings">
-                                        <p class="palmar-message palmar-warning">{{warning}}</p>
-                                    </div>
-                                    <ul class="palmar-info-list">
-                                        <li>{{notification.data.name}}</li>
-                                        <li>Price: {{notification.data.price}}</li>
-                                    </ul>
-                                    <p class="palmar-message">Ingredients:</p>
-                                    <ul class="palmar-info-list">
-                                        <li v-for="ingredient in notification.data.data.ingredients">{{ingredient}}</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </transition>
-                    </div>
-
-                    <button class="palmar-gui-button error" @click="sendHelpRequest()">
-                        ask for help
-                    </button>
-                </div>
-                <div class="home-button"></div>
-            </div>`,
-            methods: {
-                sendHelpRequest() {
-                    app.sendHelpRequest(this.palmar);
-                },
-                removeNotification(notification) {
-                    app.removeClientPalmarNotification(notification);
-                },
-                dragStart(event) {
-                    app.phoneDragging = true;
-                    event.dataTransfer.setData("text", event.target.id);
-                    event.target.classList.add('dragging');
-                },
-                dragEnd(event) {
-                    app.phoneDragging = false;
-                    event.target.classList.remove('dragging');
-                }
-            }
-        },
-        'shelf': {
-            props: ['shelf', 'visibleTab'],
-            template: `
-            <div class="device shelf">
-                <label class="device-label">Shelf {{shelf.idx}}</label>
-                
-                <div v-for="index in shelf.quantity" class="product" draggable="true" 
-                     @dragstart="dragStart($event)" @dragend="dragEnd()"
-                     @dragenter="dragEnter($event, shelf.product.idx)" @dragleave="dragLeave($event)">
-                    <img :src="shelf.product.img" draggable="false"/>
-                    <span>{{shelf.product.name}}</span>
-                </div>
-                
-                <div class="buttons" v-show="visibleTab === 'staff-view'">
-                    <button @click="updateShelf(shelf, shelf.quantity + 1)">+</button>
-                </div>
-                <div class="buttons" v-show="visibleTab === 'client-view'">
-                    <button @click="updateShelf(shelf, shelf.quantity - 1)">-</button>
-                </div>
-            </div>`,
-            methods: {
-                updateShelf(shelf, newQuantity) {
-                    app.updateShelf(shelf, newQuantity);
-                },
-                dragEnter(event, productIdx) {
-                    if (app.phoneDragging) {
-                        event.target.classList.add('dragover');
-                        event.preventDefault();
-                        app.sendProductInformationRequest(productIdx);
-                    }
-                },
-                dragLeave(event) {
-                    if (app.phoneDragging) {
-                        event.target.classList.remove('dragover');
-                        event.preventDefault();
-                    }
-                },
-                dragStart(event) {
-                    app.productDragging = true;
-                    event.dataTransfer.setData("text", event.target.id);
-                },
-                dragEnd() {
-                    app.productDragging = false;
-                }
-            }
-        },
-        'cart': {
-            template: `
-                <div class="cart device" @dragenter="dragEnter($event)" 
-                    @dragend="dragEnd($event)" @dragleave="dragEnd($event)" 
-                    @dragover="allowDrop($event)" @drop="productDropped($event)">
-                </div>`,
-            methods: {
-                dragEnter(event) {
-                    if (app.productDragging) {
-                        event.target.classList.add('hovered');
-                    }
-                },
-                dragEnd(event) {
-                    if (app.productDragging) {
-                        event.target.classList.remove('hovered');
-                    }
-                },
-                productDropped(event) {
-                    this.dragEnd(event);
-                },
-                allowDrop(event) {
-                    if (app.productDragging) {
-                        event.preventDefault();
-                    }
-                }
-            }
-        }
-    };
-}
-
-var debug;
 
 window.onload = setupPage;
