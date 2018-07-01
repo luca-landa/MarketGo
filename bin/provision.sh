2af17c9d6f5a119c121dc980a9c8df4030342d59
luca-landa <lucalanda@hotmail.it>
2018-07-01 20:41:47 +0200

    * provisioning warnings fix

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+current_folder=$(dirname ${BASH_SOURCE[0]})
+
 #update nodejs version
 sudo npm cache clean -f
 sudo npm install -g n
@@ -8,6 +10,6 @@ sudo n 10.2.1
 sudo npm install -g abbrev
 sudo npm install -g mongodb
 
-cd ..
+cd ${current_folder}/..
 sudo npm link mongodb
 sudo npm install
